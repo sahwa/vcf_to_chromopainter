58c2213c5e06f07721715b12f868fcab1517eb94
sahwa <ucbtsm8@ucl.ac.uk>
2022-05-20 16:31:02 +0100

    adding header file

--- vcf_to_chromopainter_functions.cpp
+++ vcf_to_chromopainter_functions.cpp
@@ -2,13 +2,9 @@
 #include <string>
 #include <sstream>
 #include <cmath>
+#include <vcf_to_chromopainter_functions.h>
 
 using namespace Rcpp;
-
-// double ReturnUncertainty2(Rcpp::String vcfield, int DSfield, int aa, int bb) ;
-Rcpp::NumericVector ReturnGenMap2(Rcpp::NumericMatrix recomap);
-std::string split_string_n(Rcpp::String original1, char separator, int gpField);
-std::vector<double> split_string_to_vector(Rcpp::String original1, char separator);
 
 // [[Rcpp::export]]
 Rcpp::NumericMatrix ReturnChromopainter(Rcpp::StringMatrix vcfGenotypes) {
