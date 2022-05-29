30779e13ab1b8d7022c00eeb2e93a8f65cc5e5a7
sahwa <ucbtsm8@ucl.ac.uk>
2022-05-29 13:20:50 +0100

    headers

--- vcf_to_chromopainter_functions.h
+++ vcf_to_chromopainter_functions.h
@@ -1,7 +1,9 @@
 #include <Rcpp.h>
 
 double ReturnUncertainty2(Rcpp::String vcfield, int DSfield, int aa, int bb) ;
-Rcpp::NumericVector ReturnGenMap2(Rcpp::NumericMatrix recomap);
-std::string split_string_n(Rcpp::String original1, char separator, int gpField);
-std::vector<double> split_string_to_vector(Rcpp::String original1, char separator);
 
+Rcpp::NumericVector ReturnGenMap2(Rcpp::NumericMatrix recomap);
+
+std::string split_string_n(Rcpp::String original1, char separator, int gpField);
+
+std::vector<double> split_string_to_vector(Rcpp::String original1, char separator);
