5316bed289f0cfe69b00c47ee8e428e7be225110
sam morris <samcmorris1@gmail.com>
2024-02-13 17:28:07 +0000

    make variable for running in different modes

--- src/vcf_to_chromopainter_main.R
+++ src/vcf_to_chromopainter_main.R
@@ -35,8 +35,8 @@ option_list = list(
 
 
 source_cpp_f = function() {
-	Rcpp::sourceCpp("vcf_to_chromopainter_functions.cpp")
-	cat("Successfully souced and compiled source code!\n")
+  Rcpp::sourceCpp("/well/ckb/users/aey472/program_files/vcf_to_chromopainter/src/vcf_to_chromopainter_functions.cpp")
+  cat("Successfully souced and compiled source code!\n")
 }
 
 make_recombination_map = function(geneticmap, positions) {
@@ -112,10 +112,19 @@ if (opt$uncertaintyMode == TRUE) {
 	cat("Running in Uncertainty mode!\n")
 }
 
-if (opt$uncertaintyMode == TRUE && is.null(opt$genotypelikelihoods) == TRUE) {
-	optparse::print_help(opt_parser)
-	stop("Must supply genotype likelihoods if running in uncertainty mode\n", call.=FALSE)
+if (!opt$uncertaintyMode && is.null(opt$genotypelikelihoods)) {
+  mode = "first"
+	cat("Running in classic chromopainter mode!\n")
+} else if (opt$uncertaintyMode && is.null(opt$genotypelikelihoods)) {
+  mode = "second"
+	cat("Running in uncertainty mode but not accounting for genotype likelihoods\n")
+} else if (opt$uncertaintyMode && !is.null(opt$genotypelikelihoods)) {
+  mode = "third"
+	cat("Running in uncertainty mode and also accounting for genotype likelihoods\n")
+} else {
+	stop("Combination of parameters is incorrect")
 }
+
 
 CPoutname = paste0(opt$output, ".chromopainter.inp")
 recomratesoutname = paste0(opt$output, ".recomrates.txt")
@@ -123,18 +132,22 @@ idfileout = paste0(opt$output, ".idfile.txt")
 
 source_cpp_f()
 
-
 GTs = processGenotypes(opt$genotypes)
 POS = GTs[[1]]
 GTs = GTs[[2]]
 
+cat("Making recombination rates file\n")
 recomap = make_recombination_map(opt$genmap, POS)
 fwrite(recomap, recomratesoutname, sep=" ")
+cat("Finished making recombination rates file\n")
 
+cat("Making idfile\n")
 idfile = data.table(V1 = colnames(GTs), V2 = colnames(GTs), V3 = 1)
 fwrite(idfile, idfileout, sep=" ")
+cat("Finished making idfile\n")
 
-if (opt$uncertaintyMode) {
+### typicical uncertainty mode 
+if (mode == "third") {
 	GLs = processGenotypeLikelihoods(opt$genotypelikelihoods)
 	gpdelim = GLs[[2]]
 	GPfield = GLs[[3]]
@@ -142,21 +155,33 @@ if (opt$uncertaintyMode) {
 	CPout = ReturnChromopainterUncerainty(as.matrix(GTs), as.matrix(GLs), GPfield, gpdelim)
 	POS[1] = paste0("P ", POS[1])
 	colnames(CPout) = POS
-	fwrite(CPout, opt$output, sep=" ", quote=F)
+	fwrite(CPout, CPoutname, sep=" ", quote=F)
 }
 
-if (!opt$uncertaintyMode) {
+## this is if we don't have genotypes but we need to print them out in uncertainty format ##
+## then we write out in uncertainty format but with no actual uncertainty in the values ##
+if (mode == "second") {
+	CPout = ReturnChromopainter(as.matrix(GTs))
+	CPout = as.data.table(CPout)	
+	POS[1] = paste0("P ", POS[1])
+	colnames(CPout) = POS
+	fwrite(CPout, CPoutname, sep=" ", quote=F)	
+}
+
+## just typical chromopainter with genotypes only and no uncertainty ##
+if (mode == "first") {
 	CPout = ReturnChromopainter(as.matrix(GTs))
 	CPout = as.data.table(CPout)
 	CPout = CPout[, combined := do.call(paste, c(.SD, sep = ""))][, "combined", with=F]
-	POS[1] = paste0("P POS[1]")
+	POS[1] = paste0("P ", POS[1])
 	CPout = rbindlist(list(data.table(combined = paste(POS, collapse=" ")), CPout))
-	fwrite(CPout, opt$output, sep = "\n", col.names = FALSE, quote = FALSE)
+	fwrite(CPout, cpoutname, sep = "\n", col.names = FALSE, quote = FALSE)
 }
 
-CPoutfile = readLines(opt$output)
+## write out the annoying chromopainter header lines ##
+CPoutfile = readLines(CPoutname)
 headlines = paste0(length(POS), "\n", ncol(GTs) * 2)
 CPoutfile = c(headlines, CPoutfile)
-writeLines(CPoutfile, opt$output)
+writeLines(CPoutfile, CPoutname)
 
 cat("Completed!\n")
